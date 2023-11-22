e1265106ce4f4929a13949d4d4e4789e41c60d68
zejian.su <zejian.su@starfivetech.com>
2023-11-22 19:11:01 +0800

    Change the so file and script in starfive_post_script.
    1. Remove the ipa_starfive.so.x.x.x from folder 'starfive_post_script'.
    2. Modify the ls starfive_post_script/replace_and_sign_ipa_starfive_lib.sh to
            starfive_post_script/replace_algorithm_lib.sh for it just need to replace the so
            file. And the sign file is not need to create.
    
    Signed-off-by: zejian.su <zejian.su@starfivetech.com>

--- starfive_post_script/replace_algorithm_lib.sh
+++ starfive_post_script/replace_algorithm_lib.sh
@@ -12,24 +12,20 @@
 # @desc: the 3A feature in starfive ipa is closed source, and starfive will
 #    provide a full feature ipa library. the script will replace with the
 #    starfive full feature ipa lib, and re-sign the so after build and install
-#    Usage: $0 [path], if not specify path, build/src/ipa/starfive/ is default
+#    Usage: $0 [path], if not specify path, build/src/ipa/starfive/algorithms/ is default
 # @returns: 0 if no error, non-zero if there's any error.
 
 ###############################################################################
 curr_dir=$(dirname $0)
 
-src_ipa=$curr_dir/ipa_starfive.so.0.0.1
+src_ipa=$curr_dir/starfive_isp_algorithms.so.0.0.1
 
-ipa_so=ipa_starfive.so
+ipa_so=starfive_isp_algorithms.so
 if [ $# -gt 0 ]; then
     dst_ipa=$1/$ipa_so
 else
-    dst_ipa=$curr_dir/../build/src/ipa/starfive/$ipa_so
+    dst_ipa=$curr_dir/../build/src/ipa/starfive/openAlgo/$ipa_so
 fi
-
-key_file="$curr_dir/../build/src/ipa-priv-key.pem"
-sign_file="${dst_ipa}.sign"
-sign_tool=$curr_dir/../src/ipa/ipa-sign.sh
 
 # check valid
 if [ ! -e $src_ipa ]; then
@@ -37,15 +33,7 @@ if [ ! -e $src_ipa ]; then
     exit 1
 fi
 if [ ! -e $dst_ipa ]; then
-    echo "libcamera had not be built: ipa_starfive.so not exist"
-    exit 1
-fi
-if [ ! -e $key_file ]; then
-    echo "libcamera had not be built: ipa-priv-key.pem not exist"
-    exit 1
-fi
-if [ ! -e $sign_tool ]; then
-    echo "src/ipa/ipa-sign.sh not exist "
+    echo "libcamera had not be built: starfive_isp_algorithms.so not exist"
     exit 1
 fi
 
@@ -55,10 +43,3 @@ cp $src_ipa $(dirname $dst_ipa)
 rm -rf $dst_ipa
 ln -sf $(basename $src_ipa) $dst_ipa
 
-# sign
-echo "$sign_tool $key_file $dst_ipa $sign_file"
-$sign_tool $key_file $dst_ipa $sign_file
-if [ $? -ne 0 ]; then
-    echo "ipa-sign.sh failed"
-    exit 1
-fi
